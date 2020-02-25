590872b933f36ec2da875c3545b345e2d86d7426
izen.oku <izenhuang@hotmail.com>
2020-02-25 11:54:00 -0800

    uncontrolled form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody, 
+    Form, FormGroup, Label, Input,  } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 // create navbar header to be used in all other components
@@ -7,11 +8,14 @@ import { NavLink } from 'react-router-dom';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
 
     this.toggleNav = this.toggleNav.bind(this);
-    this.state = {
-      isNavOpen: false
-    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
@@ -19,6 +23,21 @@ class Header extends React.Component {
       isNavOpen: !this.state.isNavOpen
     });
   }
+
+  toggleModal() {
+      console.log("Toggling modal")
+      this.setState({
+          isModalOpen: !this.state.isModalOpen
+      })
+  }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
 
   render() {
         return(
@@ -42,6 +61,13 @@ class Header extends React.Component {
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg">Login</span>
+                                    </Button>
+                                </NavItem> 
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -55,6 +81,32 @@ class Header extends React.Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader> 
+                    <ModalBody>
+                        {/* adding the uncontrolled form */}
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
   }
